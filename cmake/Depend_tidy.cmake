ecf4be2abbf47db78c8528e8770a8bb48b9bd601
Sylvain Fargier <fargier.sylvain@free.fr>
2017-03-26 17:33:21 +0200

    Removing C++, moving node version

--- cmake/Depend_tidy.cmake
+++
@@ -1,9 +0,0 @@
-
-include(Tools)
-
-find_xlibrary(TIDY_LIBRARIES tidy PATH_SUFFIXES "tidy")
-find_xpath(TIDY_INCLUDE_DIRS tidy.h PATH_SUFFIXES "tidy")
-
-assert(TIDY_LIBRARIES "HtmlTidy library not found")
-assert(TIDY_INCLUDE_DIRS "HtmlTidy headers not found")
-
--- cmake/Dependencies.cmake
+++
@@ -1,8 +0,0 @@
-
-include(CheckFunctionExists)
-check_function_exists(getopt_long HAVE_GETOPT_LONG)
-
-include(Depend_Qt)
-include(Depend_tidy)
-include(Depend_cppunit)
-
