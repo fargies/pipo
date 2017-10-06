2f43d9fa7ce52e1d40a0b5350d6e938fed04502a
Fargier Sylvain <fargier.sylvain@free.fr>
2017-10-06 11:51:48 +0200

    Update dependencies

--- tests/test_Generic.js
+++ tests/test_Generic.js
@@ -12,37 +12,38 @@ const
 describe('Generic', function() {
 
   _.forEach(_.keys(pipo.Registry.pipes), function(elt) {
-    if (!_.includes([ 'StdIn' ], elt)) {
-
-      it(`unconnected ${elt} terminates`, function(done) {
-        let Elt = pipo[elt];
-        assert.ok(!_.isNil(Elt));
-        assert.ok(Elt.prototype instanceof pipo.PipeElement);
-
-        var pipe = new Elt();
-        pipe.on('end', function() {
-          done();
-        });
-        pipe.start();
-      });
-
-      it(`${elt} drops empty items`, function(done) {
-        let Elt = pipo[elt];
-        let count = 0;
-        assert.ok(!_.isNil(Elt));
-        assert.ok(Elt.prototype instanceof pipo.PipeElement);
-
-        var pipe = new Elt();
-        pipe.on('item', function() {
-          count = count + 1;
-        });
-        pipe.on('end', function() {
-          assert.equal(count, 0);
-          done();
-        });
-        pipe.onItem({}); /* send an empty item */
-      });
+    if (_.includes([ 'StdIn' ], elt)) {
+      return;
     }
+
+    it(`unconnected ${elt} terminates`, function(done) {
+      let Elt = pipo[elt];
+      assert.ok(!_.isNil(Elt));
+      assert.ok(Elt.prototype instanceof pipo.PipeElement);
+
+      var pipe = new Elt();
+      pipe.on('end', function() {
+        done();
+      });
+      pipe.start();
+    });
+
+    it(`${elt} drops empty items`, function(done) {
+      let Elt = pipo[elt];
+      let count = 0;
+      assert.ok(!_.isNil(Elt));
+      assert.ok(Elt.prototype instanceof pipo.PipeElement);
+
+      var pipe = new Elt();
+      pipe.on('item', function() {
+        count = count + 1;
+      });
+      pipe.on('end', function() {
+        assert.equal(count, 0);
+        done();
+      });
+      pipe.onItem({}); /* send an empty item */
+    });
 
     it(`configuration passes through ${elt}`, function(done) {
       let Elt = pipo[elt];
